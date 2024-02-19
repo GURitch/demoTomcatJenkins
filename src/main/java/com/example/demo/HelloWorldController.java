3040ce427a09eebc6c6b18c89b1261317ebbd153
Arthur <guritch21@gmail.com>
2024-02-19 17:16:47 +0300

    change 1

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,2 +1,12 @@
-package com.example.demo;public class HelloWorldController {
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping
+    public String hello(){
+        return "Hello, world!";
+    }
 }
